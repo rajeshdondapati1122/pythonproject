fbc8c7f72ba0b35b141b74fc5e04dc3e000ef19c
rajeshdondapati1122 <163703768+rajeshdondapati1122@users.noreply.github.com>
2025-02-15 14:05:05 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.utils import add_numbers
+
+def main():
+    a, b = 5, 10
+    result = add_numbers(a, b)
+    print(f"Sum of {a} and {b} is {result}")
+
+if _name_ == "_main_":
+    main()
